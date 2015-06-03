a8f1c8df52510146383191d7e2659e13d279fe08
Thiago Poiani <thpoiani@gmail.com>
2015-06-02 21:52:55 -0300

    Argumento para arquivo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+var colors = require('colors/safe')
+var argv = require('yargs')
+  // .example('node $0 --file planet-notes-latest.osn')
+  .option({
+    'file': {
+      alias: 'f',
+      describe: 'Open an OpenStreetMap Notes file',
+      type: 'string',
+      require: true
+    }
+  })
+  .version(function () {
+    return require('../package.json').version
+  })
+  .help('help')
+  .argv
